5e395309abbb4f3b05dbcd31851fa7c1afb4c796
Marcus Hörnemann <marcushoernemann@gmail.com>
2026-02-14 15:54:14 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,26 @@
-const CACHE_NAME = 'druckkosten-v1.9.6';
+const CACHE_NAME = 'druckkosten-v1.9.7';
 const ASSETS = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './logo.png'
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting(); // Erzwingt, dass der neue Service Worker sofort aktiv wird
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  // Alten Cache löschen
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    }).then(() => self.clients.claim()) // Übernimmt sofort die Kontrolle über alle Tabs
   );
 });
 
